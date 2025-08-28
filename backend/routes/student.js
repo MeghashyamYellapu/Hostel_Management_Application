9e32ea4c67f48d9dcdcdb699b30e03486b406847
MeghaShyamy <shyamyellapu1@gmail.com>
2025-08-28 14:59:41 +0530

    solved error for new request

--- backend/routes/student.js
+++ backend/routes/student.js
@@ -1,16 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { createLeaveRequest, getStudentLeaveRequests, getSingleLeaveRequest, generateGatePass, getStudentProfile, updateStudentProfile,deleteRequest } = require('../controllers/studentController');
+const { createLeaveRequest, getStudentLeaveRequests, getSingleLeaveRequest, generateGatePass, deleteRequest } = require('../controllers/studentController');
 const { protect, studentOnly } = require('../middleware/auth');
 
 router.post('/requests', protect, studentOnly, createLeaveRequest);
 router.get('/requests', protect, studentOnly, getStudentLeaveRequests);
 router.get('/requests/:id', protect, studentOnly, getSingleLeaveRequest);
-router.post('/generate-pass/:id', protect, studentOnly, generateGatePass); // New route
-
-// Profile routes
-router.get('/profile', protect, studentOnly, getStudentProfile);
-router.put('/profile', protect, studentOnly, updateStudentProfile);
+router.post('/generate-pass/:id', protect, studentOnly, generateGatePass);
 router.delete('/requests/:id', protect, studentOnly, deleteRequest);
 
 module.exports = router;
